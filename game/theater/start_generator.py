2c2e492985b42e7d849c1dc4f598350377b3c438
Juanjo Alvarez <juanjo@juanjoalvarez.net>
2026-07-09 11:54:22 +0200

    Add F/A-18C AGM-158B JASSM-ER (SYNTAX) as an optional mod

--- game/theater/start_generator.py
+++ game/theater/start_generator.py
@@ -87,6 +87,7 @@ class ModSettings:
     fa18c_aargm_er: bool = False
     fa18c_aargm_er_realistic: bool = False
     fa18c_lrasm: bool = False
+    fa18c_jassm: bool = False
     f22_raptor: bool = False
     f84g_thunderjet: bool = False
     f100_supersabre: bool = False
